2faf201b4baa594e96b217f7a61c7b0019687450
ShervilG <wintergpt45@gmail.com>
2022-09-11 22:16:51 +0530

    Initialised Skeleton Project

---
+++ src/main/java/com/shervilg/spinboard/SpinboardApplication.java
@@ -0,0 +1,12 @@
+package com.shervilg.spinboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpinboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpinboardApplication.class, args);
+	}
+}
