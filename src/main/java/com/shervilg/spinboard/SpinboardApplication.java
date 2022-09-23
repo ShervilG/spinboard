f579316fcdc77d7f6750d5f61cf7fa0fb889d215
ShervilG <wintergpt45@gmail.com>
2022-09-23 17:02:02 +0530

    Integrated notification scheduler

--- src/main/java/com/shervilg/spinboard/SpinboardApplication.java
+++ src/main/java/com/shervilg/spinboard/SpinboardApplication.java
@@ -7,6 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @SpringBootApplication
 public class SpinboardApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpinboardApplication.class, args);
 	}
