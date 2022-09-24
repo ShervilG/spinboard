e7816c54caf817b06fd39f7417db27e72ee1654e
ShervilG <wintergpt45@gmail.com>
2022-09-24 22:27:09 +0530

    Added help command listener

--- src/main/java/com/shervilg/spinboard/bot/common/CommandConstant.java
+++ src/main/java/com/shervilg/spinboard/bot/common/CommandConstant.java
@@ -1,8 +1,12 @@
 package com.shervilg.spinboard.bot.common;
 
-public class CommandConstant {
-  private static final String SPACE = " ";
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
+public class CommandConstant {
+  // Common strings
+  public static final String SPACE = " ";
   public static final String COMMAND_PREFIX = "=>";
 
   // Help Commands
@@ -11,4 +15,9 @@ public class CommandConstant {
   // Birthday Commands
   public static final String BIRTHDAY_COMMAND_PREFIX = COMMAND_PREFIX + "bday";
   public static final String BIRTHDAY_LIST_COMMAND = BIRTHDAY_COMMAND_PREFIX + SPACE + "list";
+
+  public static Map<String, String> HELP_MAP = Map.of(
+      HELP_COMMAND, "For viewing all help commands",
+      BIRTHDAY_LIST_COMMAND, "For getting the list of all Birthdays"
+  );
 }
--- src/main/java/com/shervilg/spinboard/bot/listener/HelpMessageListener.java
+++ src/main/java/com/shervilg/spinboard/bot/listener/HelpMessageListener.java
@@ -13,13 +13,17 @@ public class HelpMessageListener extends MessageCreateListenerTemplate {
   protected boolean isValidMessageCreateEvent() {
     return messageCreateEvent.getMessage().getChannel() != null
         && !StringUtils.isEmpty(messageCreateEvent.getMessage().getContent())
-        && messageCreateEvent.getMessage().getContent().startsWith(CommandConstant.HELP_COMMAND)
+        && messageCreateEvent.getMessage().getContent().strip().equals(CommandConstant.HELP_COMMAND)
         && messageCreateEvent.getMessage().getChannel().getId() == DiscordChannel.COMMANDS_CHANNEL.getChannelId();
   }
 
   @Override
   protected void performActionOnMessageCreateEvent() {
-    EmbedBuilder helpMessageEmbed = new EmbedBuilder().addField("List Of Commands", "Under construction !");
+    EmbedBuilder helpMessageEmbed = new EmbedBuilder();
+    helpMessageEmbed.setTitle("List Of Commands");
+
+    CommandConstant.HELP_MAP.forEach(helpMessageEmbed::addField);
+
     messageCreateEvent.getChannel().sendMessage(helpMessageEmbed);
   }
 }
