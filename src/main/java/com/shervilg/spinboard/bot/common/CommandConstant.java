a565a373106670d5ecf3a243773acd068bd9e42f
ShervilG <wintergpt45@gmail.com>
2022-09-25 12:29:36 +0530

    Added buttons to help

--- src/main/java/com/shervilg/spinboard/bot/common/CommandConstant.java
+++ src/main/java/com/shervilg/spinboard/bot/common/CommandConstant.java
@@ -14,8 +14,13 @@ public class CommandConstant {
   public static final String BIRTHDAY_COMMAND_PREFIX = COMMAND_PREFIX + "bday";
   public static final String BIRTHDAY_LIST_COMMAND = BIRTHDAY_COMMAND_PREFIX + SPACE + "list";
 
+  // Command Maps
   public static Map<String, String> HELP_MAP = Map.of(
       HELP_COMMAND, "For viewing all help commands",
       BIRTHDAY_LIST_COMMAND, "For getting the list of all Birthdays"
+  );
+  public static Map<String, String> BUTTON_ENABLED_COMMANDS = Map.of(
+      HELP_COMMAND, "Help",
+      BIRTHDAY_LIST_COMMAND, "List Bdays"
   );
 }
--- src/main/java/com/shervilg/spinboard/bot/listener/HelpMessageListener.java
+++ src/main/java/com/shervilg/spinboard/bot/listener/HelpMessageListener.java
@@ -1,11 +1,19 @@
 package com.shervilg.spinboard.bot.listener;
 
+import io.lettuce.core.protocol.Command;
 import org.apache.commons.lang3.StringUtils;
+import org.javacord.api.entity.message.MessageBuilder;
+import org.javacord.api.entity.message.component.Button;
+import org.javacord.api.entity.message.component.LowLevelComponent;
+import org.javacord.api.entity.message.embed.Embed;
 import org.springframework.stereotype.Service;
 import com.shervilg.spinboard.common.enums.DiscordChannel;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
 import com.shervilg.spinboard.bot.common.CommandConstant;
 import com.shervilg.spinboard.bot.template.MessageCreateListenerTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class HelpMessageListener extends MessageCreateListenerTemplate {
@@ -19,11 +27,26 @@ public class HelpMessageListener extends MessageCreateListenerTemplate {
 
   @Override
   protected void performActionOnMessageCreateEvent() {
-    EmbedBuilder helpMessageEmbed = new EmbedBuilder();
-    helpMessageEmbed.setTitle("List Of Commands");
+    MessageBuilder message = new MessageBuilder();
+    EmbedBuilder embed = new EmbedBuilder();
+    List<LowLevelComponent> buttons = new ArrayList<>();
 
-    CommandConstant.HELP_MAP.forEach(helpMessageEmbed::addField);
+    embed.setTitle("List of Commands");
 
-    messageCreateEvent.getChannel().sendMessage(helpMessageEmbed);
+    CommandConstant.HELP_MAP.forEach((key, value) -> {
+      embed.addField(key, value);
+
+      if (CommandConstant.BUTTON_ENABLED_COMMANDS.get(key) != null) {
+        buttons.add(Button.primary(key, CommandConstant.BUTTON_ENABLED_COMMANDS.get(key)));
+      }
+    });
+
+    message.addEmbed(embed);
+
+    if (buttons.size() != 0) {
+      message.addActionRow(buttons.toArray(new LowLevelComponent[0]));
+    }
+
+    message.send(messageCreateEvent.getChannel());
   }
 }
